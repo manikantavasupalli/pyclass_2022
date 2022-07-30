c872b36ba2b36529ba2d1abfbbbcffd60ecbd635
manikanta vasupalli <manikanta.vasupalli@innovaccer.com>
2022-07-30 08:18:38 +0530

    pushing few changes related fun

--- Basics/function.py
+++ Basics/function.py
@@ -1,8 +1,57 @@
-def greet(name, counter):
-    print("Hey {} ... How are you doing".format(name))
-    if counter < 10:
-        greet("Mani", counter+1)
+# def greet(name, counter):
+#     print("Hey {} ... How are you doing".format(name))
+#     if counter < 10:
+#         greet("Mani", counter+1)
+
+
+# greet("Rob", 5)
+
+
+# def afun(name):
+#     print("Hey {} how are you doing".format(name))
+
+
+# afun("Mathew")
+# afun("Sudhir")
+# afun("nathan")
+
+
+# def check_even(num):
+#     if num%2==0:
+#         return True
+#     else:
+#         return False
+
+# x = 11
+# if check_even(x):
+#     print("{} is even".format(x))
+# else:
+#     print("{} is odd".format(x))
+
+
+# def sample(name, age, location, counter):
+#     print("Hi this is {} am {} years old and am from {}".format(name, age, location))
+#     print(counter)
+#     if counter > 0:
+#         counter = counter - 2 
+#         sample(name, age, location, counter)
+
+# a=10
+# b=a*10
+# print(b)
+# # sample("robin", 200, 'USA', 6)
+# sample("Bob", 20, 'Japan', 2)
 
 
 
-greet("Rob", 0)
+# def refun():
+#     print(1)
+#     print(2)
+#     print(3)
+#     return 10000
+#     print(4)
+#     print(5)
+#     print(6)
+
+
+# print(refun())
